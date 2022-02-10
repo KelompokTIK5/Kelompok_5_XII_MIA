59f4030aecc611a44d471b09c45d4802f9d556b8
KelompokTIK5 <98506180+KelompokTIK5@users.noreply.github.com>
2022-02-10 16:41:14 +0800

    Add files via upload

---
+++ Form3.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form3
+
+    Private Sub DateTimePicker1_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles DTTanggal.ValueChanged, DTTanggal.ValueChanged, Enabled.ValueChanged
+
+    End Sub
+
+    Private Sub Form3_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        DTTanggal.Format = DateTimePickerFormat.Short
+        DTTanggal.CustomFormat = "dd/mm/yyyy"
+        DTTanggal.Value = Format(Now)
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        TextBox1.Text = DTTanggal.Text
+
+    End Sub
+End Class
